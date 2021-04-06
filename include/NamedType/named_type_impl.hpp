6a6215abed56a92f3b64cffc6859ae06219fcc4d
Aneek Datta <aneek.datta@cartenav.com>
2021-04-06 12:21:19 -0300

    make private inheritance

--- include/NamedType/named_type_impl.hpp
+++ include/NamedType/named_type_impl.hpp
@@ -63,7 +63,7 @@ using IsNotReference = typename std::enable_if<!std::is_reference<T>::value, voi
 
 template <typename T, typename Parameter, template <typename> class... Skills>
 class FLUENT_EBCO NamedType
-    : public _IsNamedType
+    : private _IsNamedType
     , public Skills<NamedType<T, Parameter, Skills...>>...    
 {
 public:
